11c6288973a73d4b75bad4c5f6444a6926ff3320
midoon <harunkoding@gmail.com>
2024-11-03 17:05:46 +0700

    Feat(UI): fix row ui teacher

--- resources/views/admin/teacher.blade.php
+++ resources/views/admin/teacher.blade.php
@@ -4,12 +4,17 @@
         <!-- Header -->
         <header class="flex items-center justify-between">
             <h1 class="text-3xl font-semibold">Data Guru</h1>
+            <a href="#">
+                <div class="px-5 py-2 bg-simipa-2 rounded-xl">
+                    <h3 class="text-simipa-6">Tambah Guru</h3>
+                </div>
+            </a>
         </header>
         <hr class="my-4">
 
         <div>
-            <div class="relative overflow-x-auto">
-                <table class="w-full text-sm text-left rtl:text-right  rounded-">
+            <div class="relative overflow-x-auto rounded-lg">
+                <table class="w-full text-sm text-left rtl:text-right">
                     <thead class="text-xs text-simipa-1 uppercase bg-gray-300">
                         <tr>
                             <th scope="col" class="px-6 py-3">
@@ -23,6 +28,9 @@
                             </th>
                             <th scope="col" class="px-6 py-3">
                                 Akses
+                            </th>
+                            <th scope="col" class="px-6 py-3">
+                                Edit
                             </th>
                         </tr>
                     </thead>
@@ -40,28 +48,43 @@
                             <td class="px-6 py-4">
                                 $2999
                             </td>
+                            <td class="px-6 py-4">
+                                <div class="flex ">
+                                    <button>
+                                        <svg class="w-6 h-6 text-gray-800 mx-1" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
+                                        </svg>
+                                    </button>
+                                    <button onclick="return confirmDeletion()">
+                                        <svg class="w-6 h-6 text-red-700" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" width="24" height="24"
+                                            fill="none" viewBox="0 0 24 24">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2"
+                                                d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
+                                        </svg>
+                                    </button>
+                                </div>
+                            </td>
                         </tr>
-                        <tr class="bg-simipa-6 border-b ">
-                            <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap">
-                                Apple MacBook Pro 17"
-                            </th>
-                            <td class="px-6 py-4">
-                                Silver
-                            </td>
-                            <td class="px-6 py-4">
-                                Laptop
-                            </td>
-                            <td class="px-6 py-4">
-                                $2999
-                            </td>
-                        </tr>
-
                     </tbody>
                 </table>
             </div>
 
         </div>
     </x-navbar-admin>
+
+
+
+    <script>
+        function confirmDeletion() {
+            return confirm("Apakah Anda yakin ingin menghapusnya?");
+        }
+    </script>
 
 
 </x-layout>
