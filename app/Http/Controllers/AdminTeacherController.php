805d207e19b673cdc892c7de46f8515eed6d6b47
midoon <harunkoding@gmail.com>
2024-11-06 21:07:08 +0700

    Fix(controller): Teacher controller store try catch

--- app/Http/Controllers/AdminTeacherController.php
+++ app/Http/Controllers/AdminTeacherController.php
@@ -18,12 +18,21 @@ class AdminTeacherController extends Controller
     public function store(Request $request){
         $roles = $request->roles ?? ['guru'];
 
-        $teacher = Teacher::create([
+        try {
+            Teacher::create([
             'name' => $request->name,
             'role' => $roles,
             'nik' => $request->nik,
             'gender' => $request->gender,
         ]);
+             return redirect("/admin/teacher");
+        } catch (QueryException $e){
+            if ($e->errorInfo[1] == 1062)//kode mysql untuk duplicate data
+            {
+                 return back()->withErrors(['nik' => "NIK: $request->nik sudah terdaftar."])->withInput();
+            }
+            return back()->withErrors(['error' => 'Terjadi kesalahan saat mengupdate data.'])->withInput();
+        }
 
 
 
@@ -45,7 +54,7 @@ class AdminTeacherController extends Controller
                 'nik' => $request->nik,
                 'gender' => $request->gender,
             ]);
-             return redirect("/admin/teacher")->with('success', 'Data guru berhasil diupdate.');
+             return redirect("/admin/teacher");
         } catch (QueryException $e){
             if ($e->errorInfo[1] == 1062)//kode mysql untuk duplicate data
             {
