53dd420e27ace55ed577f5a8e64fd8196cb6e130
midoon <harunkoding@gmail.com>
2024-11-09 21:51:24 +0700

    Fix(UI): admin sideebar

--- resources/views/admin/grade/index.blade.php
+++ resources/views/admin/grade/index.blade.php
@@ -42,11 +42,11 @@
                                         {{ $grade->name }}
                                     </td>
                                     <td class="px-6 py-4">
-                                        <div class="flex ">
+                                        <div class="flex gap-3">
                                             <button>
-                                                <svg class="w-6 h-6 text-gray-800 mx-1" aria-hidden="true"
-                                                    xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                    fill="none" viewBox="0 0 24 24">
+                                                <svg class="w-6 h-6 text-gray-800 hover:text-simipa-2 mx-1"
+                                                    aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                                                    height="24" fill="none" viewBox="0 0 24 24">
                                                     <path stroke="currentColor" stroke-linecap="round"
                                                         stroke-linejoin="round" stroke-width="2"
                                                         d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
@@ -56,9 +56,10 @@
                                                 @csrf
                                                 @method('DELETE')
                                                 <button type="submit" onclick="return confirmDeletion()">
-                                                    <svg class="w-6 h-6 text-red-700" aria-hidden="true"
-                                                        xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                        fill="none" viewBox="0 0 24 24">
+                                                    <svg class="w-6 h-6 text-gray-800 hover:text-red-700"
+                                                        aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
+                                                        width="24" height="24" fill="none"
+                                                        viewBox="0 0 24 24">
                                                         <path stroke="currentColor" stroke-linecap="round"
                                                             stroke-linejoin="round" stroke-width="2"
                                                             d="M5 7h14m-9 3v8m4-8v8M10 3h4a1 1 0 0 1 1 1v3H9V4a1 1 0 0 1 1-1ZM6 7h12v13a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7Z" />
@@ -108,11 +109,11 @@
                                     123
                                 </td>
                                 <td class="px-6 py-4">
-                                    <div class="flex ">
+                                    <div class="flex gap-3">
                                         <button>
-                                            <svg class="w-6 h-6 text-gray-800 mx-1" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                fill="none" viewBox="0 0 24 24">
+                                            <svg class="w-6 h-6 text-gray-800 hover:text-simipa-2 mx-1"
+                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                                                height="24" fill="none" viewBox="0 0 24 24">
                                                 <path stroke="currentColor" stroke-linecap="round"
                                                     stroke-linejoin="round" stroke-width="2"
                                                     d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
@@ -122,7 +123,7 @@
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" onclick="return confirmDeletion()">
-                                                <svg class="w-6 h-6 text-red-700" aria-hidden="true"
+                                                <svg class="w-6 h-6 text-grey-800 hover:text-red-700" aria-hidden="true"
                                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                     fill="none" viewBox="0 0 24 24">
                                                     <path stroke="currentColor" stroke-linecap="round"
--- resources/views/admin/teacher/index.blade.php
+++ resources/views/admin/teacher/index.blade.php
@@ -56,12 +56,12 @@
                                     {{ implode(', ', $teacher->role) }}
                                 </td>
                                 <td class="px-6 py-4">
-                                    <div class="flex ">
+                                    <div class="flex gap-3">
                                         <button onclick="openEditModal({{ $teacher->id }})"
                                             data-target="#editModalTeacher{{ $teacher->id }}">
-                                            <svg class="w-6 h-6 text-gray-800 mx-1" aria-hidden="true"
-                                                xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                                                fill="none" viewBox="0 0 24 24">
+                                            <svg class="w-6 h-6 text-gray-800 hover:text-simipa-2 mx-1"
+                                                aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24"
+                                                height="24" fill="none" viewBox="0 0 24 24">
                                                 <path stroke="currentColor" stroke-linecap="round"
                                                     stroke-linejoin="round" stroke-width="2"
                                                     d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
@@ -71,7 +71,7 @@
                                             @csrf
                                             @method('DELETE')
                                             <button type="submit" onclick="return confirmDeletion()">
-                                                <svg class="w-6 h-6 text-red-700" aria-hidden="true"
+                                                <svg class="w-6 h-6 text-grey-800 hover:text-red-700" aria-hidden="true"
                                                     xmlns="http://www.w3.org/2000/svg" width="24" height="24"
                                                     fill="none" viewBox="0 0 24 24">
                                                     <path stroke="currentColor" stroke-linecap="round"
