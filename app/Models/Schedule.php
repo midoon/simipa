a088d18bc7432888c5e70674b868813029dc5e9b
midoon <harunkoding@gmail.com>
2024-11-30 10:06:05 +0700

    Fix(admin-schedule-model): change time format

--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -7,7 +7,32 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Schedule extends Model
 {
+
+    protected $casts = [
+        'start_time' => 'datetime:H:i',
+        'end_time' => 'datetime:H:i',
+    ];
    protected $guarded = ['id'];
+
+   public function getStartTimeAttribute($value)
+    {
+        return \Carbon\Carbon::createFromFormat('H:i:s', $value)->format('H:i');
+    }
+
+    public function getEndTimeAttribute($value)
+    {
+        return \Carbon\Carbon::createFromFormat('H:i:s', $value)->format('H:i');
+    }
+
+    public function setStartTimeAttribute($value)
+    {
+        $this->attributes['start_time'] = \Carbon\Carbon::createFromFormat('H:i', $value)->format('H:i:s');
+    }
+
+    public function setEndTimeAttribute($value)
+    {
+        $this->attributes['end_time'] = \Carbon\Carbon::createFromFormat('H:i', $value)->format('H:i:s');
+    }
 
    public function group(): BelongsTo{
     return $this->belongsTo(Group::class);
