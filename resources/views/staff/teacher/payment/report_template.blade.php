368b359bff89c72e7ca01bd6bbf5a7b62b917083
midoon <harunkoding@gmail.com>
2025-01-25 09:55:38 +0700

    Refactor(teacher-payment-report): style css

--- resources/views/staff/teacher/payment/report_template.blade.php
+++ resources/views/staff/teacher/payment/report_template.blade.php
@@ -5,72 +5,137 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pembayaran | Rekapitulasi</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            padding: 10px;
+        }
+
+        .header {
+            text-align: center;
+            padding: 20px;
+        }
+
+        .header h1 {
+            font-weight: bold;
+            font-size: 24px;
+            margin: 10px 0;
+        }
+
+        .info {
+            margin-bottom: 30px;
+        }
+
+        .info .row {
+            display: flex;
+            justify-content: space-between;
+            margin-bottom: 5px;
+            font-weight: bold;
+            font-size: 14px;
+        }
+
+        .info .row span {
+            text-align: left;
+            white-space: nowrap;
+        }
+
+        .table-container {
+            border: 1px solid #ccc;
+            border-radius: 5px;
+            overflow: hidden;
+        }
+
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            font-size: 14px;
+        }
+
+        thead {
+            background-color: #708871;
+            color: white;
+        }
+
+        th {
+            border: 1px solid #ccc;
+            padding: 10px;
+            text-align: center;
+        }
+
+        th.no-column {
+            width: 5%;
+        }
+
+        th.tagihan-column,
+        th.status-column {
+            width: 25%;
+        }
+
+        tbody tr {
+            background-color: #f9f9f9;
+            border: 1px solid #ccc;
+        }
+
+        td {
+            border: 1px solid #ccc;
+            padding: 8px;
+            text-align: center;
+        }
+    </style>
 </head>
 
-<body style="font-family: Arial, sans-serif; margin: 20px; padding: 10px;">
-    <div style="text-align: center; padding: 20px;">
-        <h1 style="font-weight: bold; font-size: 24px; margin: 10px 0;">REKAPITULASI PEMBAYARAN SISWA</h1>
-        <h1 style="font-weight: bold; font-size: 24px; margin: 10px 0;">MI PANCASILA MOJOSARI MOJOKERTO</h1>
+<body>
+    <div class="header">
+        <h1>REKAPITULASI PEMBAYARAN SISWA</h1>
+        <h1>MI PANCASILA MOJOSARI MOJOKERTO</h1>
     </div>
 
-    <div style="margin-bottom: 30px;">
-        <div>
-            <div
-                style="display: flex; justify-content: space-between; margin-bottom: 5px; font-weight: bold; font-size: 14px;">
-                <span style="width:10%; text-align: left; white-space: nowrap;">Rombel</span>
-                <span style="width: 1%; text-align: center;">:</span>
-                <span style="width: 89%; text-align: left;">{{ $group }}</span>
-            </div>
-            <div
-                style="display: flex; justify-content: space-between; margin-bottom: 5px; font-weight: bold; font-size: 14px;">
-                <span style="width:10%; text-align: left; white-space: nowrap;">Jenis</span>
-                <span style="width: 1%; text-align: center;">:</span>
-                <span style="width: 89%; text-align: left;">{{ $paymentType }}</span>
-            </div>
-            <div
-                style="display: flex; justify-content: space-between; margin-bottom: 5px; font-weight: bold; font-size: 14px;">
-                <span style="width:10%; text-align: left; white-space: nowrap;">Tagihan per Kelas</span>
-                <span style="width: 1%; text-align: center;">:</span>
-                <span style="width: 89%; text-align: left;">{{ $totalAmount }}</span>
-            </div>
-            <div
-                style="display: flex; justify-content: space-between; margin-bottom: 5px; font-weight: bold; font-size: 14px;">
-                <span style="width: 10%; text-align: left; white-space: nowrap;">Terbayar Per Kelas</span>
-                <span style="width: 1%; text-align: center;">:</span>
-                <span style="width: 89%; text-align: left;">{{ $totalPaidFeeAmount }}</span>
-            </div>
+    <div class="info">
+        <div class="row">
+            <span style="width:10%;">Rombel</span>
+            <span style="width: 1%; text-align: center;">:</span>
+            <span style="width: 89%;">{{ $group }}</span>
+        </div>
+        <div class="row">
+            <span style="width:10%;">Jenis</span>
+            <span style="width: 1%; text-align: center;">:</span>
+            <span style="width: 89%;">{{ $paymentType }}</span>
+        </div>
+        <div class="row">
+            <span style="width:10%;">Tagihan per Kelas</span>
+            <span style="width: 1%; text-align: center;">:</span>
+            <span style="width: 89%;">{{ $totalAmount }}</span>
+        </div>
+        <div class="row">
+            <span style="width:10%;">Terbayar Per Kelas</span>
+            <span style="width: 1%; text-align: center;">:</span>
+            <span style="width: 89%;">{{ $totalPaidFeeAmount }}</span>
         </div>
     </div>
 
-
-
-    <div style="border: 1px solid #ccc; border-radius: 5px; overflow: hidden;">
-        <table style="width: 100%; border-collapse: collapse; font-size: 14px;">
-            <thead style="background-color: #708871; color: white;">
+    <div class="table-container">
+        <table>
+            <thead>
                 <tr>
-                    <th style="border: 1px solid #ccc; padding: 10px; text-align: center; width: 5%;">No</th>
-                    <th style="border: 1px solid #ccc; padding: 10px; text-align: center;">Nama</th>
-                    <th style="border: 1px solid #ccc; padding: 10px; text-align: center; width: 25%;">Tagihan</th>
-                    <th style="border: 1px solid #ccc; padding: 10px; text-align: center; width: 25%;">Status</th>
+                    <th class="no-column">No</th>
+                    <th>Nama</th>
+                    <th class="tagihan-column">Tagihan</th>
+                    <th class="status-column">Status</th>
                 </tr>
             </thead>
             <tbody>
                 @foreach ($studentData as $data)
-                    <tr style="background-color: #f9f9f9; border: 1px solid #ccc;">
-                        <td style="border: 1px solid #ccc; padding: 8px; text-align: center;">{{ $loop->iteration }}
-                        </td>
-                        <td style="border: 1px solid #ccc; padding: 8px; text-align: center;">{{ $data['name'] }}</td>
-                        <td style="border: 1px solid #ccc; padding: 8px; text-align: center;">
-                            {{ $data['remainingAmount'] }}</td>
-                        <td style="border: 1px solid #ccc; padding: 8px; text-align: center;">{{ $data['status'] }}</td>
-
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $data['name'] }}</td>
+                        <td>{{ $data['remainingAmount'] }}</td>
+                        <td>{{ $data['status'] }}</td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
-
-
 </body>
 
 </html>
